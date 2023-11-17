cd723998d91f11191a39220624bfdd931c1ac353
dadiers <dalton-adiers@outlook.com>
2023-11-17 13:43:25 -0300

    Selection Sort

---
+++ Sorts/SelectionSort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+vector <int> ordenar;
+
+void geraRandom(int n){
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> distribution(-10000,10000);
+    for(int i=0; i<n; i++){
+        ordenar.push_back(distribution(gen));
+    }
+}   
+
+void selectionSort(int n){
+    int indiceMinimo;
+    for(int i=0;i<n-1;i++){
+        indiceMinimo=i;
+        for(int j=i+1;j<n;j++){
+            if(ordenar[j]<ordenar[indiceMinimo])indiceMinimo=j;
+        }
+        if(indiceMinimo!=i)swap(ordenar[indiceMinimo],ordenar[i]);
+    }
+}
+
+int main(){
+    cout << "Bem vindo à ordenação por SelectionSort! Digite o número de elementos que deseja ordenar: ";
+    int n; cin >> n; cout << "\n";
+    geraRandom(n);
+    auto start = chrono::high_resolution_clock::now();
+    selectionSort(n);
+    auto end = chrono::high_resolution_clock::now();
+    auto durationSec = chrono::duration<double>(end - start).count();
+    for(int i=0; i<n; i++){
+        cout << ordenar[i] << " ";
+    }
+    cout << "\n";
+    cout << "Tempo de execução do SelectionSort: " << fixed << setprecision(3) << durationSec << " segundos.\n";
+    return 0;
+}
